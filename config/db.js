1a8a97849a553d5e2915f2cf33cef21452bbb567
Navin <navint@metropolia.fi>
2026-09-14 14:04:41 +0300

    This is iteration 2

--- config/db.js
+++ config/db.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/web-dev");
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
- 
-module.exports = connectDB;
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error.message);
+  });
 
+module.exports = mongoose;
